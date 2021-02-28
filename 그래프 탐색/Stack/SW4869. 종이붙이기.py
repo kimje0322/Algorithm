8740c2b2aeeba8663cb181b30cb978c68ec6082f
kimje <happyday751@naver.com>
2021-03-01 00:28:06 +0900

    0228 Graph Traversal

--- 그래프 탐색/Stack/SW4869. 종이붙이기.py
+++ 그래프 탐색/Stack/SW4869. 종이붙이기.py
@@ -1,3 +1,9 @@
-for t in range(1, int(input()+1)):
-    n = int(input())
-    
+def f(n):
+    if n < 2:
+        return 1
+    else:
+        return f(n-1) + 2*f(n-2)
+
+for t in range(1, int(input())+1):
+    n = int(input()) // 10
+    print('#%d %d' % (t, f(n)))
