2675d6dab981db81ca1ccf12997061bb6732324c
kimje <happyday751@naver.com>
2021-03-16 00:21:58 +0900

    0316 Backtracking

--- 백트래킹/BJ16987. 계란으로 계란치기.py
+++ 백트래킹/BJ16987. 계란으로 계란치기.py
@@ -1,43 +1,89 @@
 import sys
 input = sys.stdin.readline
 
-def backtracking(l,cur,data):
-    global cnt
-    if l >= N:
-        cnt = max(cnt, cur)
-        return
-    # d 내구성 w 무게
-    if data[l][0] > 0:
-        all_broken = True
-        for r in range(N):
-            if r == l or data[r][0] <= 0:
-                continue
-            all_broken = False
-            data[l][0] -= data[r][1]
-            data[r][0] -= data[l][1]
-            n_cur = cur
-            n_cur += 1 if data[l][0] <= 0 else 0
-            n_cur += 1 if data[r][0] <= 0 else 0
-            backtracking(l+1, n_cur, data)
-            data[l][0] += data[r][1]
-            data[r][0] += data[l][1]
-        if all_broken:
-            backtracking(l+1, cur, data)
-    else:
-        backtracking(l+1, cur, data)
-
-
 N = int(input())
-cnt = 0
-data = [[] for _ in range(N)]
-for i in range(N):
-    data[i] = list(map(int, input().split()))
-backtracking(0,0,data)
-print(cnt)
+egg_dur = []
+egg_kg = []
+for _ in range(N):
+    dur, kg = map(int, input().split())
+    egg_dur.append(dur)
+    egg_kg.append(kg)
+maxV = 0
+
+def egg_break(idx, egg_dur, cnt):
+    global maxV
+    if idx == N:
+        maxV = max(maxV, cnt)
+        return
+    if egg_dur[idx] <= 0:
+        egg_break(idx+1, egg_dur, cnt)
+        # 현재 idx 계란이 깨진 경우 다음 계란으로 넘기는 부분인데
+        # 재귀함수 호출한 다음에 return 하는 이유가 뭐지?
+        return
+    break_1more = False
+
+    # idx=0 일 때 i = 1 ... 오른쪽으로 순차적으로만 이동가능한데
+    # for문 쓰면 idx=0일때 i = 1,2,3... 전체 탐색하는거 아닌가?
+    for i in range(N):
+        new_egg_dur = egg_dur[:]
+        new_cnt = cnt
+        if i != idx and egg_dur[idx] > 0 and egg_dur[i] > 0:
+            break_1more = True
+            new_egg_dur[idx] -= egg_kg[i]
+            new_egg_dur[i] -= egg_kg[idx]
+            if new_egg_dur[idx] <= 0:
+                new_cnt += 1
+            if new_egg_dur[i] <= 0:
+                new_cnt += 1
+            egg_break(idx+1, new_egg_dur, new_cnt)
+    # 이 부분 어떤 경우인지 이해 잘 안감
+    # 깨진 경우 다음 계란
+    if not break_1more:
+        egg_break(idx+1, egg_dur, cnt)
+
+egg_break(0,egg_dur[:],0)
+print(maxV)
+
+
+# def backtracking(l,cur,data):
+#     global cnt
+#     if l >= N:
+#         cnt = max(cnt, cur)
+#         return
+#     # d 내구성 w 무게
+#     if data[l][0] > 0:
+#         all_broken = True
+#         for r in range(N):
+#             if r == l or data[r][0] <= 0:
+#                 continue
+#             all_broken = False
+#             data[l][0] -= data[r][1]
+#             data[r][0] -= data[l][1]
+#             n_cur = cur
+#             n_cur += 1 if data[l][0] <= 0 else 0
+#             n_cur += 1 if data[r][0] <= 0 else 0
+#             backtracking(l+1, n_cur, data)
+#             data[l][0] += data[r][1]
+#             data[r][0] += data[l][1]
+#         if all_broken:
+#             backtracking(l+1, cur, data)
+#     else:
+#         backtracking(l+1, cur, data)
+#
+#
+# N = int(input())
+# cnt = 0
+# data = [[] for _ in range(N)]
+# for i in range(N):
+#     data[i] = list(map(int, input().split()))
+# backtracking(0,0,data)
+# print(cnt)
 
 
 # 현재 문제점: data를 변경시켜도 원래 data 리스트를 참조하고 있어서
 # if len(data) > 1: 구문에서 빠져나오지 못하고 계속 걸린다.
+# 슬라이싱으로 금방 해결
+# 다음 문제점: 다음 함수 호출 조건, 함수 종료 조건 설정
 
 # def backtracking(cur):
 #     global cnt, data
