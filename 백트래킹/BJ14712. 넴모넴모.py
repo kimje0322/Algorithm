0c579119fdd13b5ba9c95dc37819a4dad287f35b
kimje <happyday751@naver.com>
2021-03-17 22:53:35 +0900

    0317 Brute Force

--- 백트래킹/BJ14712. 넴모넴모.py
+++ 백트래킹/BJ14712. 넴모넴모.py
@@ -10,35 +10,45 @@ board = [[0]*M for _ in range(N)]
 dx, dy = [1,1,0], [0,1,1]
 
 def game(idx):
-    global cnt, stack
+    global cnt, r, c
     nonCount = False
+    # val = idx-1
+    # r1 = val/M
+    # c1 = val/M
+    if 1 <= r < N and 1 <= c < M:
+       if board[r-1][c-1] and board[r-1][c] and board[r][c-1]:
+           return
+
     if idx == N*M:
-        for ele in stack:
-            if nonCount:
-                return
-            cur_r, cur_c = ele
-            twotwo = 0
-            for d in range(3):
-                if (cur_r+dx[d], cur_c+dy[d]) in stack:
-                    twotwo += 1
-                if twotwo == 3:
-                    nonCount = True
-                    break
-        if nonCount:
-            cnt -= 1
+        cnt += 1
         return
 
     for i in range(N):
         for j in range(M):
-            if not board[i][j]:
-                board[i][j] = 1
-                stack.append((i,j))
-                game(idx+1)
-                board[i][j] = 0
-                stack.pop()
-stack = []
-cnt = 2**(N*M)
-# board [0,0] = 1
+            board[i][j] = 1
+            r, c = i, j
+            game(idx+1)
+            board[i][j] = 0
+            r, c = i, j
+            game(idx+1)
+
+cnt = 0
+r,c = 0,0
 game(0)
 print(cnt)
 
+#     if idx == N*M:
+#         for ele in stack:
+#             if nonCount:
+#                 return
+#             cur_r, cur_c = ele
+#             twotwo = 0
+#             for d in range(3):
+#                 if (cur_r+dx[d], cur_c+dy[d]) in stack:
+#                     twotwo += 1
+#                 if twotwo == 3:
+#                     nonCount = True
+#                     break
+#         if nonCount:
+#             cnt -= 1
+#         return
