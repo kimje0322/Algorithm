1aed9d4528c2ca7f99c8bde502c952bbd0070035
kimje <happyday751@naver.com>
2021-05-01 23:37:34 +0900

    0501 Dynamic Programing

--- 동적계획법/BOJ2602.  돌다리 건너기.py
+++ 동적계획법/BOJ2602.  돌다리 건너기.py
@@ -1,27 +1,30 @@
+# dp
+
+# 완전탐색
 # 돌다리 idx, 정답(word) idx, angle/devil
 def move(f,c,r):
     global ans
-    if f == len(word):
-        ans += 1
-        return
+    if c == len(word):
+        return 1
+    if dp[f][c][r] > -1:
+        return dp[f][c][r]
+    total = 0
     # devil
     if r == 0:
-        for i in range(c, len(devil)):
-            print(word[f], devil[i])
-            if word[f] == devil[i]:
-                move(i+1, f+1, 1)
+        for i in range(f, len(devil)):
+            if word[c] == devil[i]:
+                total += move(i+1, c+1, 1)
     # angel <= r:1
-    else:
-        for i in range(c, len(angel)):
-            print(word[f], angel[i])
-            if word[f] == angel[i]:
-                move(i+1, f+1, 0)
-    return
+    if r == 1 or c == 0:
+        for i in range(f, len(angel)):
+            if word[c] == angel[i]:
+                total += move(i+1, c+1, 0)
+    dp[f][c][r] = total
+    return total
 
-ans = 0
 word = input()
 devil = input()
 angel = input()
 n = len(angel)
-move(0,0,0)
-
+dp = [[[-1 for _ in range(3)] for _ in range(n)] for _ in range(n)]
+print(move(0,0,0))
--- 동적계획법/PRO. N으로 표현.py
+++ 동적계획법/PRO. N으로 표현.py
@@ -4,3 +4,7 @@ input = sys.stdin.readline
 def solution(N, number):
     answer = 0
     return answer
+
+# N = 2
+# dp = [[[0 for _ in range(3)] for _ in range(N)] for _ in range(N)]
+# print(dp)
