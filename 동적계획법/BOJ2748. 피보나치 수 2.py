5ee886da9eb1a3ce7d02b59005984aaaaf02c845
kimje <happyday751@naver.com>
2021-05-15 23:30:43 +0900

    0515 Dynamic Programing

--- 동적계획법/BOJ2748. 피보나치 수 2.py
+++ 동적계획법/BOJ2748. 피보나치 수 2.py
@@ -1,7 +1,11 @@
 def fibo(n):
-    if n <= 1:
-        return n
-    return fibo(n-1) + fibo(n-2)
+    for i in range(n+1):
+        if i <= 1:
+            dp[i] = i
+        else:
+            dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
 
 n = int(input())
+dp = [0]*(n+1)
 print(fibo(n))
