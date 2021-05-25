6c076522f64edb161dd3bea19980712f47344743
kimje <happyday751@naver.com>
2021-05-25 23:29:33 +0900

    0525 DP

--- 동적계획법/BOJ2156. 포도주 시식.py
+++ 동적계획법/BOJ2156. 포도주 시식.py
@@ -1,8 +1,13 @@
-n = int(input())
-wine = []
-for _ in range(n):
-    wine.append(input())
-dp = [wine[0], wine[0]+wine[1]]+[0]*(n-2)
+n=int(input())
+wine=[0]*10000
+for i in range(n):
+  wine[i]=int(input())
 
-for i in range(3,n+1):
-    pass
+d=[0]*10000
+d[0]=wine[0]
+d[1]=wine[0]+wine[1]
+d[2]=max(wine[2]+wine[0], wine[2]+wine[1], d[1])
+for i in range(3,n):
+  d[i]=max(wine[i]+d[i-2], wine[i]+wine[i-1]+d[i-3], d[i-1])
+
+print(max(d))
